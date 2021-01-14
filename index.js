0ffbfdc8edf2ec4269c8d9d392e8daa6458dfbea
dainbrownlow08 <dain.brownlow@outlook.com>
2021-01-13 16:34:48 -0800

    commit tree a4c7c8c354fc8b32c538e8d782f257b40e839b90

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+function idToPage(json){
+    let id = json.id;
+    let b = document.querySelector('body');
+    b.innerHTML = "";
+    b.innerHTML = id;
+}
+
+function errorM(message){
+    let b = document.querySelector('body');
+    b.innerHTML = "";
+    b.innerHTML = message;
+}
+
+function submitData(name,email){
+
+    let data = {
+        name: name,
+        email: email
+    };
+    
+    let obj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+    
+    return fetch("http://localhost:3000/users", obj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        idToPage(object);
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        errorM(error.message);
+    }); 
+    
+
+}
